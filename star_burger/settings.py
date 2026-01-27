68ec1f5ca715174cfe6174ebe2536e81b37c2796
jendox <jendox1985@gmail.com>
2026-01-27 16:12:35 +0300

    add docker

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -12,7 +12,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '0.0.0.0'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
